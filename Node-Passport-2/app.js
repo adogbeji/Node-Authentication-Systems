7ed8a69cd1355a82b1ad0680600c0ffd8c6bc470
Benjamin Akponasa <baka_1993@yahoo.co.uk>
2021-02-19 20:18:58 +0000

    Add database to Node Passport 2

--- Node-Passport-2/app.js
+++ Node-Passport-2/app.js
@@ -14,9 +14,23 @@ app.use(express.static(`${__dirname}/public`));
 app.use('/account', express.static(`${__dirname}/public`));
 app.use('/account/details', express.static(`${__dirname}/public`));
 
+const DB = 'mongodb+srv://ben:uNEFt4nW3Ok2kpAF@authentication-app.beqyt.mongodb.net/userDB?retryWrites=true&w=majority';
+
+mongoose.connect(DB, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true
+})
+.then(() => {
+  console.log('MongoDB Connected!');
+})
+.catch(err => {
+  console.log(err);
+});
+
 const port = process.env.PORT;
 
-// For Next Time: Connect to Atlas Cluster and continue building server
+// For Next Time: Create Schema & Model and continue building server
 
 app.listen(port, (req, res) => {
   console.log(`Listening on port ${port}...`);
